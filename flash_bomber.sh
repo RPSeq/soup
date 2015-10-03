e90586e0db1e7c24da6d9fe83918bb1ec533dc88
SmithRyan <ryan.smith.p@gmail.com>
2015-10-02 19:43:58 -0500

    added real usage loops to flash_bomber

--- flash_bomber.sh
+++ flash_bomber.sh
@@ -10,6 +10,7 @@
 #INPUT FILE DIRS
 INPUT=/gscmnt/gc2802/halllab/rsmith/Hippocampal_L1/FASTQ/original
 SINGLE_IN=${INPUT}/single
+BULK_IN=${INPUT}/bulk
 
 #OUTPUT DIRS
 OUTPUT_DIR=/gscmnt/gc2802/halllab/rsmith/Hippocampal_L1/FASTQ/flash
@@ -24,17 +25,45 @@ SINGLE_LOGS=${OUTPUT_DIR}/single_logs
 SRC=/gscmnt/gc2802/halllab/rsmith/Hippocampal_L1/src
 
 #make sure job group is set to reasonable number (i have brought the cluster to a grinding halt before)
-bgmod -L 30 /rsmith/flash
+bgmod -L 40 /rsmith/flash
+
+################################################
+###test
+################################################
 
 #test on one small single sample
-SAMPLES=("Cortex.Single.WGS.CTRL-45.Neuron-#4")
+# SAMPLES=("Cortex.Single.WGS.CTRL-45.Neuron-#4")
 
+# for SAMPLE in $SAMPLES; do
+#     FQ1=${SINGLE_IN}/${SAMPLE}.s_1_1_sequence.txt.gz;
+#     FQ2=${SINGLE_IN}/${SAMPLE}.s_1_2_sequence.txt.gz;
+#     bomb -t 8 -m 16 -J $SAMPLE -g /rsmith/flash -q long -o ${SINGLE_LOGS}/${SAMPLE}.out -e ${SINGLE_LOGS}/${SAMPLE}.err \
+#     "bash ${SRC}/flash.sh -d ${SINGLE_OUT} -s $SAMPLE $FQ1 $FQ2"
+# done
+
+################################################
+###bulk
+################################################
+
+#Grab sample names for bulk sequencing
+# SAMPLES=$(ls $BULK_IN | sed -e 's/.s_1_._sequence.txt.gz//g' | uniq)
+
+# for SAMPLE in $SAMPLES; do
+#     FQ1=${BULK_IN}/${SAMPLE}.s_1_1_sequence.txt.gz;
+#     FQ2=${BULK_IN}/${SAMPLE}.s_1_2_sequence.txt.gz;
+#     bomb -t 8 -m 16 -J $SAMPLE -g /rsmith/flash -q long -o ${BULK_LOGS}/${SAMPLE}.out -e ${BULK_LOGS}/${SAMPLE}.err \
+#     "bash ${SRC}/flash.sh -d ${BULK_OUT} -s $SAMPLE $FQ1 $FQ2"
+# done
+
+################################################
+###single
+################################################
+
+Grab sample names for single sequencing
+SAMPLES=$(ls $SINGLE_IN | sed -e 's/.s_1_._sequence.txt.gz//g' | uniq)
 for SAMPLE in $SAMPLES; do
     FQ1=${SINGLE_IN}/${SAMPLE}.s_1_1_sequence.txt.gz;
     FQ2=${SINGLE_IN}/${SAMPLE}.s_1_2_sequence.txt.gz;
     bomb -t 8 -m 16 -J $SAMPLE -g /rsmith/flash -q long -o ${SINGLE_LOGS}/${SAMPLE}.out -e ${SINGLE_LOGS}/${SAMPLE}.err \
     "bash ${SRC}/flash.sh -d ${SINGLE_OUT} -s $SAMPLE $FQ1 $FQ2"
 done
-
-
-
